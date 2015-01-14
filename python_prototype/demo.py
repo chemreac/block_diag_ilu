1182f7d502acb6f06c4b61efce72ac7e2c5969ab
Björn Dahlgren <bjodah@gmail.com>
2015-01-14 16:17:10 +0100

    Enable demo to investigate ndiag

--- python_prototype/demo.py
+++ python_prototype/demo.py
@@ -15,6 +15,9 @@ hence not suitable for benchmarking.
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from future.builtins import *
+from math import exp
+
+from itertools import product
 
 import argh
 import numpy as np
@@ -45,9 +48,9 @@ def get_test_system(N, n, ndiag, main_diag_factor, off_diag_factor, seed):
         x_blk.append(lu_solve(lu_factor(cur_block), b[slc]))
 
     for di in range(ndiag):
-        sub_ = rnd((N-di-1)*n)*off_diag_factor**(di+1)
-        sup_ = rnd((N-di-1)*n)*off_diag_factor**(di+1)
-        for i in range(n*(N-1)):
+        sub_ = rnd((N-di-1)*n)*off_diag_factor
+        sup_ = rnd((N-di-1)*n)*off_diag_factor
+        for i in range(n*(N-di-1)):
             A[(di+1)*n + i, i] = sub_[i]
             A[i, (di+1)*n + i] = sup_[i]
         sub.append(sub_)
@@ -60,9 +63,9 @@ def get_test_system(N, n, ndiag, main_diag_factor, off_diag_factor, seed):
     return A, b, x_ref, x_ilu, x_blk
 
 
-def main(N=5, n=5, ndiag=1, main_diag_factor=1.0, off_diag_factor=1.0,
-         base_seed=0, seed_range=1, fact_pow2_min=0, fact_pow2_max=1, plot=False,
-         npows=0, savefig='None'):
+def main(N=32, n=32, ndiag=1, main_diag_factor=1.0, off_diag_factor=1.0,
+         base_seed=0, seed_range=1, fact_pow2_min=4, fact_pow2_max=18,
+         plot=False, npows=0, scan_ndiag=False, savefig='None'):
     """
     Ax = b
     """
@@ -73,8 +76,21 @@ def main(N=5, n=5, ndiag=1, main_diag_factor=1.0, off_diag_factor=1.0,
     ilu_rmsd, blk_rmsd = [], []
     superiority = []
 
-    for seed in range(seed_range):
+    if scan_ndiag:
+        if seed_range != 1:
+            raise ValueError("Cannot plot mulitple seeds and scan ndiag")
+        ndiag_range = range(1, ndiag+1)
+    else:
+        ndiag_range = [ndiag]
+    combos = product(ndiag_range, range(seed_range))
 
+    #for seed in range(seed_range):
+    #seed = base_seed
+    #for ndiag in ndiag_range:
+
+    nseries = 0
+    for ndiag, seed in combos:
+        nseries += 1
         ilu_rmsd_local, blk_rmsd_local = [], []
 
         for diag_fact_pow in factors:
@@ -89,7 +105,7 @@ def main(N=5, n=5, ndiag=1, main_diag_factor=1.0, off_diag_factor=1.0,
             ilu_rmsd_local.append(rms(ilu_err))
             blk_rmsd_local.append(rms(blk_err))
 
-        if plot and seed_range == 1:
+        if plot and seed_range == 1 and not scan_ndiag:
             import matplotlib.pyplot as plt
 
             if npows == 1:
@@ -111,44 +127,75 @@ def main(N=5, n=5, ndiag=1, main_diag_factor=1.0, off_diag_factor=1.0,
                 plt.semilogy(blk_rmsd, label="Block RMSD")
                 plt.legend()
                 plt.show()
+
         ilu_rmsd.append(np.array(ilu_rmsd_local))
         blk_rmsd.append(np.array(blk_rmsd_local))
         superiority.append(np.array(blk_rmsd_local) / np.array(ilu_rmsd_local))
         if np.any(superiority[-1] < 1e-3):
             print('1000 x inferior:', seed)
 
-    if plot and seed_range > 1:
+    if plot and (seed_range > 1 or scan_ndiag):
         import matplotlib.pyplot as plt
 
         plt.figure(figsize=(6, 14))
-        plot_kwargs = dict(alpha=0.1, linewidth=0.2)
+
+        if scan_ndiag:
+            plot_kwargs = {}
+        else:
+            decay = exp(-((seed_range-1)/50.0))
+            plot_kwargs = dict(alpha=1.0 - 0.9*(1-decay), linewidth=0.2 + 0.8*decay)
 
         ax = plt.subplot(3, 1, 1)
         ax.set_xscale('log', basex=10)
         ax.set_yscale('log', basey=10)
-        for series in ilu_rmsd:
-            plt.plot(2**factors, series, 'b', **plot_kwargs)
+        clr = lambda idx, rgb: [1.0 - (nseries-idx)/float(nseries) if clridx==rgb else 0.0 for clridx in range(3)]
+        for si, series in enumerate(ilu_rmsd):
+            if scan_ndiag:
+                c = clr(si, 2)  # blue
+                lbl = str(ndiag_range[si])
+            else:
+                c = 'b'
+                lbl = None
+            plt.plot(2**factors, series, color=c, label=lbl, **plot_kwargs)
         plt.title("ILU")
         plt.xlabel("weight")
         plt.ylabel("RMSD")
+        if scan_ndiag:
+            plt.legend(loc='best')
 
         ax = plt.subplot(3, 1, 2)
         ax.set_xscale('log', basex=10)
         ax.set_yscale('log', basey=10)
-        for series in blk_rmsd:
-            plt.plot(2**factors, series, 'g', **plot_kwargs)
+        for si, series in enumerate(blk_rmsd):
+            if scan_ndiag:
+                c = clr(si, 1)  # green
+                lbl = str(ndiag_range[si])
+            else:
+                c = 'g'
+                lbl = None
+            plt.plot(2**factors, series, color=c, label=lbl, **plot_kwargs)
         plt.title("Block RMSD")
         plt.xlabel("weight")
         plt.ylabel("RMSD")
+        if scan_ndiag:
+            plt.legend(loc='best')
 
         ax = plt.subplot(3, 1, 3)
         ax.set_xscale('log', basex=10)
         ax.set_yscale('log', basey=10)
-        for series in superiority:
-            plt.plot(2**factors, series, 'k', **plot_kwargs)
+        for si, series in enumerate(superiority):
+            if scan_ndiag:
+                c = clr(si, 0)  # red
+                lbl = str(ndiag_range[si])
+            else:
+                c = 'k'
+                lbl = None
+            plt.plot(2**factors, series, color=c, label=lbl, **plot_kwargs)
         plt.title("BLOCK RMSD / ILU RMSD")
         plt.xlabel("weight")
         plt.ylabel("RMSD fraction")
+        if scan_ndiag:
+            plt.legend(loc='best')
 
         plt.tight_layout()
 
