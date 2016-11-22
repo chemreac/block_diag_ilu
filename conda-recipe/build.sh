2d3e0f9b56b6e408d05ff8736daacde8362fa7df
Björn Dahlgren <bjodah@gmail.com>
2016-11-22 12:48:17 +0100

    Prepare for v0.2.3 release

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+BLOCK_DIAG_ILU_LAPACK=openblas CPLUS_INCLUDE_PATH=${PREFIX}/include ${PYTHON} setup.py build
 ${PYTHON} setup.py build
 ${PYTHON} setup.py install
--- conda-recipe/run_test.py
+++ conda-recipe/run_test.py
@@ -3,6 +3,10 @@
 
 import os
 from block_diag_ilu import get_include
-assert os.listdir(get_include())[0] == 'block_diag_ilu.hpp'
+from block_diag_ilu.tests.test_block_diag_ilu import test_ilu_solve__1
+
+test_ilu_solve__1()  # rest of test suite run as CI
+
+assert 'block_diag_ilu.hpp' in os.listdir(get_include())
 path = os.path.join(get_include(), 'block_diag_ilu.hpp')
 assert open(path, 'rt').readline().startswith('#pragma once')
