2d9460637be5709dfcef4f4f1296ffdee58f2b77
Björn Ingvar Dahlgren <bjodah@gmail.com>
2019-05-20 15:05:09 +0200

    import __version__

--- block_diag_ilu/__init__.py
+++ block_diag_ilu/__init__.py
@@ -4,6 +4,7 @@ block_diag_ilu is a C++ implementation of an incomplete LU factorization, ILU(0)
 """
 
 from __future__ import (absolute_import, division, print_function)
+from ._release import __version__
 
 from ._block_diag_ilu import (
     Compressed, Compressed_from_dense, Compressed_from_data,
